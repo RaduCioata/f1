941448a429d7dade6ee9abf27545c6843e5891c9
Radu <itziradu@gmail.com>
2025-05-26 17:55:25 +0300

    k

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,4 +1,4 @@
-import nextJest from 'next/jest.js';
+const nextJest = require('next/jest');
 
 const createJestConfig = nextJest({
   dir: './',
@@ -42,4 +42,4 @@ const customJestConfig = {
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
-export default createJestConfig(customJestConfig); 
+module.exports = createJestConfig(customJestConfig); 
